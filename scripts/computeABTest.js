e3c4c97753493144e2a81be077406f635a2be5e1
lutaoact <lutaoact@hotmail.com>
2016-10-06 08:41:50 +0800

    add message fromusername

--- scripts/computeABTest.js
+++ scripts/computeABTest.js
@@ -13,12 +13,18 @@ const redisService = _u.service('redis');
 
 const openId = 'o0zx1s4KfSsw4yOo74g1o3P78DW4';
 
+function getMessage(cb) {
+  _u.mySeries({
+    allMessage: (_cb) => {
+      messageService.getSubscribeMessage(null, _cb);
+    },
+    inviteMessage: (_cb, ret) => {
+      messageService.getSubscribeMessage({invitation: true}, _cb);
+    },
+  },cb);
+};
 
-_u.mySeries({
-  allMessage: (_cb) => {
-    messageService.getSubscribeMessage(null, console.log);
-  },
-  inviteMessage: (_cb, ret) => {
-    messageService.getSubscribeMessage({invitation: true}, console.log);
-  },
-}, cb);
+getMessage((err, ret) => {
+    console.log(ret.allMessage.length);
+    console.log(ret.inviteMessage.length);
+})
--- services/message.js
+++ services/message.js
@@ -19,20 +19,20 @@ function getSubscribeMessage(opt, cb) {
   // 返回所有被邀请关注的消息
   if (opt && opt.invitation) {
     Message.find({'content.Event':'subscribe', 'content.EventKey': /qrscene_/i},
-      'content.FromUserName EventKey', function(err, messages) {
-      cb(messages);
+      'content.FromUserName content.EventKey', function(err, messages) {
+      cb(err, messages);
     });
   }
   // 返回所有没有被邀请的关注消息
   else if (opt && !opt.invitation) {
     Message.find({'content.Event':'subscribe', 'content.EventKey': ''}, 'content.FromUserName', function(err, messages) {
-      cb(messages);
+      cb(err, messages);
     });
   }
   // 返回所有关注消息
   else {
     Message.find({'content.Event':'subscribe'}, 'content.FromUserName', function(err, messages) {
-      cb(messages);
+      cb(err, messages);
     });
   }
 }
