35f58cf060021fe16226751e1e42181c943b479a
ZYH <sqhtiamo@163.com>
2016-10-06 09:35:35 +0800

    feat(ABTestComputation) - add user model

--- scripts/computeABTest.js
+++ scripts/computeABTest.js
@@ -26,6 +26,21 @@ function getMessage(cb) {
     users: (_cb, ret) => {
       userService.getAllUsers({}, 'openid incrId threshold', _cb);
     },
+    // 计算达标人数
+    quanlification: (_cb, ret) => {
+      let ob = ret.inviteMessage.map((value) => {
+        let incrId = +value.content.EventKey.replace(/^qrscene_/, '');
+        let userInfo = _u.getObjFromArrByKeyName(ret.users, incrId, 'incrId');
+        return {
+          incrId: incrId,
+          threshold: userInfo.threshold
+        }
+      }).reduce((pre, cur) => {
+        pre[cur.incrId] ? pre[cur.incrId]++ : pre[cur.incrId] = 1;
+        return pre;
+      }, {});
+      console.log(ob);
+    }
     threshold: (_cb, ret) => {
       let noInvitation = {};
       for (let i = 0; i < ret.noInviteMessage.length; i++) {
