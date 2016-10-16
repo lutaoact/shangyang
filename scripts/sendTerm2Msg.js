a5f2a3f0e61d0573b24d987ffc7e59aa7b7c9a7a
ZYH <sqhtiamo@163.com>
2016-10-16 09:26:37 +0800

    send Term 3 invitation msg

--- scripts/sendTerm2Msg.js
+++ scripts/sendTerm2Msg.js
@@ -12,15 +12,19 @@ const weixin = require('../common/weixin');
 const redisService = _u.service('redis');
 
 const openIds = [
-  // {"id": "o0zx1syKzlgUKh34grllBy6ZkHcc"},
-  // {"id": "o0zx1sxpWoJwZeTlGwzBeVvpcBNg"},
-  // {"id": "o0zx1s9VkoIk4YoTxnr3VOLYyuUc"},
-  // {"id": "o0zx1s_BBc7aF9uM5OTDvnZJupeI"},
-  // {"id": "o0zx1swqwcOzZOwTur5tQBtnmx-c"},
-  // {"id": "o0zx1s__ELNLOIVWLm_JyuRAOIEI"},
-  // {"id": "o0zx1s0gQv8Hva5D8VcskwRzcOkI"},
-  // {"id": "o0zx1s3LCRIMAIDBX755zv4R3D3Y"},
-{"id":"o0zx1sxqDA-nkvz9_wr-PxxcqXuU"}
+{"id": "o0zx1s7gMqY9MSfcpXZDRUyhqWkU"},
+{"id": "o0zx1s_knfht-4jFkOmYHg3uHTgc"},
+{"id": "o0zx1s9JiPDdh0WN4ht2gsCfJzjo"},
+{"id": "o0zx1s_TmxrQz0kbPjcX5z-SFfpw"},
+{"id": "o0zx1s0ND_JUEj8CDYOafTlwbffE"},
+{"id": "o0zx1s4fiSJe4EPoPuHvztWeFU1Q"},
+{"id": "o0zx1s4RLOBnFqQiPTYB7uraIalc"},
+{"id": "o0zx1s0o0qzRicTizLbZZLy3ISEg"},
+{"id": "o0zx1syPmVegAixGStjgRLDtrsRI"},
+{"id": "o0zx1s8IYi3_kuEaYD56hJW1MmAQ"},
+{"id": "o0zx1s2EW76-h9Zot0m5VVAFpXMg"},
+{"id": "o0zx1s2Hs_lC9ZFCZk7q_NxdOwFU"},
+{"id": "o0zx1s4KfSsw4yOo74g1o3P78DW4"},
 ];
 
 
@@ -39,8 +43,9 @@ function sendMsg(openIdObj, cb) {
     },
     sendMsg: (_cb, ret) => {
       console.log(ret.rank);
-      loggerD.write('[Send Message]', "Term 2 Message openId" , openId, 'rank', ret.rank);
-      weixin.sendMsgToQualifiedInviter(openId, ret.rank, _cb);
+      loggerD.write('[Send Message]', "Term 3 Message openId" , openId, 'rank', ret.rank);
+      // weixin.sendMsgToQualifiedInviter(openId, ret.rank, _cb);
+      weixin.sendImage(openid, `./groupQrCode/term2.jpg`, _cb);
     },
   }, _u.delayRun(cb));
 }
